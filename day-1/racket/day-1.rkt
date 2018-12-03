c4e5322f1a5ab8ab0f0bce5c89759a33cf2b392a
J. Dennis <echo.mirage.rk@gmail.com>
2018-12-03 13:53:56 -0500

    Add .gitignore and work so far That's day 1, part 1 in Racket

---
+++ day-1/racket/day-1.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+; Name of the input file
+(define INPUT-FILENAME "../input.txt")
+
+; Read each line from an input port into a list
+(define (lines->list input)
+  (for/list ([line (in-lines input)])
+    line))
+
+; Read the lines from our input file into a list
+(define lines (call-with-input-file INPUT-FILENAME
+                                    lines->list))
+
+; Iterate the list, convert to numbers, and add them into
+; an accumulator. We don't need to do anything about the +s
+(define (calc-final-freq freq-list)
+  (for/fold ([freq 0])
+            ([delta-str (in-list freq-list)])
+    
+    (define delta (string->number delta-str))
+    (+ freq delta)))
+
+; Call the function with our list
+(define final-frequency (calc-final-freq lines))
+
+; Print the frequency all neat-like
+(printf "Frequency at end of file: ~a" final-frequency)
