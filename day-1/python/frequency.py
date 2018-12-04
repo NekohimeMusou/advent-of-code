f462eeda4ddc97100fcd90beaa3732fccee46a50
J. Dennis <echo.mirage.rk@gmail.com>
2018-12-04 13:29:10 -0500

    Day 1 Part 1 done in Python

--- day-1/python/frequency.py
+++ day-1/python/frequency.py
@@ -1,13 +1,26 @@
-INPUT_PATH = "day-1/input.txt"
+INPUT_PATH = "../input.txt"
 
 
 def main():
     lines = get_input()
 
+    final_freq = calc_final_frequency(lines)
+
+    print("Ending frequency: {0}".format(final_freq))
+
 
 def get_input(path=INPUT_PATH):
     with open(path) as f:
-        return f.readlines()
+        return [int(x) for x in f.readlines()]
+
+
+def calc_final_frequency(delta_list, initial_freq=0):
+    current_freq = initial_freq
+
+    for delta in delta_list:
+        current_freq += delta
+
+    return current_freq
 
 
 if __name__ == "__main__":
