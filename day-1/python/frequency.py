380c9f7a682e06a53a6924ee7ba2f9d6e46ee7a5
J. Dennis <echo.mirage.rk@gmail.com>
2018-12-04 15:06:15 -0500

    Add better comments to day 1

--- day-1/python/frequency.py
+++ day-1/python/frequency.py
@@ -13,18 +13,19 @@ def main():
     print("Ending frequency: {0}\nFirst frequency repeated twice: {1}".format(final_freq, first_rep))
 
 
+# Open the input file and convert each line to a number.
+# int() strips whitespace and understands the + symbol
 def get_input(path=INPUT_PATH):
     with open(path) as f:
+        # This here's a list comprehension
         return [int(x) for x in f.readlines()]
 
 
+# Calculate the final frequency after all changes in the list.
+# Since our starting frequency is known and the operations are transitive
+# we can just sum the array for this
 def calc_final_frequency(delta_list, initial_freq=0):
-    current_freq = initial_freq
-
-    for delta in delta_list:
-        current_freq += delta
-
-    return current_freq
+    return sum(delta_list, initial_freq)
 
 
 def find_repetition(delta_list, initial_freq=0):
@@ -32,6 +33,8 @@ def find_repetition(delta_list, initial_freq=0):
     freqs_tested = {initial_freq}
     current_freq = initial_freq
 
+    # itertools.cycle gives us a generator that works like an infinitely-repeating list
+    # This won't actually loop forever since we return when we find our target
     for delta in cycle(delta_list):
         current_freq += delta
 
