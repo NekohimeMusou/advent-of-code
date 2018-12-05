5723616d9494d28277ea0d54bc92e7d8319bce52
J. Dennis <echo.mirage.rk@gmail.com>
2018-12-05 18:23:58 -0500

    Finished Day 2 (Python)

--- day-2/python/part-2.py
+++ day-2/python/part-2.py
@@ -1,23 +1,47 @@
+from itertools import combinations
+
 INPUT_PATH = "../input.txt"
+SAMPLE_DATA = ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz']
 
 
 def main():
-    with open(INPUT_PATH) as f:
-        lines = f.readlines()
+    lines = get_input()
 
-    prototype_ids = find_prototype_ids(lines)
+    prototype_string = find_prototype_string(lines)
+
+    print('The prototype string is:', prototype_string)
 
 
-def find_prototype_ids(id_list):
+def get_input(path=INPUT_PATH):
+    with open(path) as f:
+        # There shouldn't be any duplicates but this will filter any out
+        return set(f.readlines())
+
+
+def find_prototype_string(id_list):
     """Find the two box ids that contain the prototype fabric.
 
-    Returns a tuple containing the two strings.
+    Returns the common characters between the two box IDs.
 
     Params:
-    id_list -- a sequence containing ids to check, which should be strings
+    id_list -- a sequence containing ids to check, which should be unique strings
     """
 
-    return None, None
+    # combinations gives us every combination of n items from the list. Perfect! <3
+    for id_1, id_2 in combinations(id_list, 2):
+        mismatches = []
+
+        for i in range(len(id_1)):
+            if id_1[i] != id_2[i]:
+                if mismatches:  # We already found a mismatch
+                    break
+                mismatches.append(i)
+        else:
+            # Only reached if we finish with 1 mismatch
+            n = mismatches[0]
+            return ''.join((id_1[0:n], id_1[n+1:len(id_1)]))
+
+    return None
 
 
 if __name__ == "__main__":
