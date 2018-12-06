64c4252d1bfa499feb1f3736843c6e2694be63fc
J. Dennis <echo.mirage.rk@gmail.com>
2018-12-06 15:07:12 -0500

    Done with parsing code for Day 3 Part 1

--- day3/part_1.py
+++ day3/part_1.py
@@ -5,10 +5,28 @@ INPUT_PATH = "input.txt"
 # grammar: #{CLAIM_ID} @ {X}, {Y}: {W}x{H}
 
 
-class Claim:
-    def __init__(self, claim_id, x1, y1, x2, y2):
-        self.claim_id = claim_id
+def main():
+    claim_list = get_input()
 
+    # http://tryalgo.org/en/geometry/2016/06/25/union-of-rectangles/
+    # https://cp-algorithms.com/data_structures/segment_tree.html
+    # https://kartikkukreja.wordpress.com/2014/11/09/a-simple-approach-to-segment-trees/
+
+    # First, divide into y-intervals
+    # Then sweep line from left; when line touches the left side of a rect, increment counter(s) for its interval
+    # When line leaves the right side, decrement the counter
+
+
+def get_input(path=INPUT_PATH):
+    with open(path) as f:
+        lines = [Claim.from_string(line) for line in f]
+
+    # Invalid lines (blanks) will be None so filter them out
+    return [claim for claim in lines if claim is not None]
+
+
+class Rect:
+    def __init__(self, x1, y1, x2, y2):
         # Top left coords
         self.x1 = x1
         self.y1 = y1
@@ -17,7 +35,17 @@ class Claim:
         self.x2 = x2
         self.y2 = y2
 
-    regex = re.compile(r"#\s*(\d+)\s*@\s*(\d+),\s*(\d+)\s*:\s*(\d+)\s*x\s*(\d+)")
+    def intersects(self, other):
+        return self.x1 < other.x2 and self.x2 > other.x1 and self.y1 < other.y2 and self.y2 > other.y1
+
+
+class Claim:
+    def __init__(self, claim_id, x1, y1, x2, y2):
+        self.claim_id = claim_id
+
+        self.rect = Rect(x1, y1, x2, y2)
+
+    regex = re.compile(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)")
 
     @classmethod
     def from_string(cls, s):
@@ -31,3 +59,7 @@ class Claim:
             return Claim(claim_id, x1, y1, x2, y2)
 
         return None
+
+
+if __name__ == '__main__':
+    main()
--- day3/tests.py
+++ day3/tests.py
@@ -5,19 +5,34 @@ from day3.part_1 import Claim
 class TestPart1(unittest.TestCase):
     def setUp(self):
         self.sample = (
-            ('#1 @ 1, 3: 4x4', Claim(1, 1, 3, 1+4, 3+4)),
+            ('#1 @ 1,3: 4x4', Claim(1, 1, 3, 1+4, 3+4)),
             ('#2 @ 3,1: 4x4', Claim(2, 3, 1, 3+4, 1+4)),
             ('#3 @ 5,5: 2x2', Claim(3, 5, 5, 5+2, 5+2))
         )
 
     def test_regex(self):
-        test_attrs = ('claim_id', 'x1', 'y1', 'x2', 'y2')
+        rect_attrs = ('x1', 'y1', 'x2', 'y2')
 
         for string, claim2 in self.sample:
             claim1 = Claim.from_string(string)
 
-            for attr in test_attrs:
-                self.assertEqual(getattr(claim1, attr, None), getattr(claim2, attr, None))
+            self.assertEqual(claim1.claim_id, claim2.claim_id)
+
+            for attr in rect_attrs:
+                self.assertEqual(getattr(claim1.rect, attr, None), getattr(claim2.rect, attr, None))
+
+    def test_intersect(self):
+        claim1, claim2, claim3 = [x[1] for x in self.sample]
+
+        # Sample claims 1 and 2 should intersect
+        self.assertTrue(claim1.rect.intersects(claim2.rect))
+        self.assertTrue(claim2.rect.intersects(claim1.rect))
+
+        # Sample claim 3 should NOT intersect either 1 or 2
+        self.assertFalse(claim1.rect.intersects(claim3.rect))
+        self.assertFalse(claim2.rect.intersects(claim3.rect))
+        self.assertFalse(claim3.rect.intersects(claim1.rect))
+        self.assertFalse(claim3.rect.intersects(claim2.rect))
 
 
 if __name__ == '__main__':
