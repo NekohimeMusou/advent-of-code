a2494fc9a2214cfcea3c5537bceb617b3744ede7
J. Dennis <echo.mirage.rk@gmail.com>
2018-12-05 21:19:26 -0500

    Tweaked unit tests

--- day3/tests.py
+++ day3/tests.py
@@ -1,16 +1,14 @@
 import unittest
 from day3.part_1 import Claim
 
-SAMPLE_DATA = [
-    ('#1 @ 1, 3: 4x4', Claim(1, 1, 3, 1+4, 3+4)),
-    ('#2 @ 3,1: 4x4', Claim(2, 3, 1, 3+4, 1+4)),
-    ('#3 @ 5,5: 2x2', Claim(3, 5, 5, 5+2, 5+2))
-]
-
 
 class TestPart1(unittest.TestCase):
     def setUp(self):
-        self.sample = SAMPLE_DATA
+        self.sample = (
+            ('#1 @ 1, 3: 4x4', Claim(1, 1, 3, 1+4, 3+4)),
+            ('#2 @ 3,1: 4x4', Claim(2, 3, 1, 3+4, 1+4)),
+            ('#3 @ 5,5: 2x2', Claim(3, 5, 5, 5+2, 5+2))
+        )
 
     def test_regex(self):
         test_attrs = ('claim_id', 'x1', 'y1', 'x2', 'y2')
