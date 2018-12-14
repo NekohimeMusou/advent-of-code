366b712e46174789941023b9eb4a41a90b0ba162
J. Dennis <echo.mirage.rk@gmail.com>
2018-12-14 14:27:36 -0500

    Day 5 Part 1 unit test passes

--- day5/part_1.py
+++ day5/part_1.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 from day5.common import get_input
 
 
@@ -6,11 +8,26 @@ def main():
 
     reacted_polymer = react_polymer(polymer)
 
-    print('Polymer chain after reaction:', reacted_polymer, '\nUnits in new chain:', len(reacted_polymer))
+    print('Polymer chain after reaction:', reacted_polymer,
+          '\nUnits in new chain:', len(reacted_polymer))
 
 
 def react_polymer(polymer):
-    return polymer
+    """React the polymer with a brute force method.
+
+    Just iterate the possible character pairs and remove them, then check whether the
+    new string is the same as the old one."""
+
+    pairs = [''.join(s) for s in permutations('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 2)
+             if s[0].casefold() == s[1].casefold() and s[0].islower() != s[1].islower()]
+
+    while True:
+        old_polymer = polymer
+
+        for pair in pairs:
+            polymer = polymer.replace(pair, '')
+        if old_polymer == '' or old_polymer == polymer:
+            return polymer
 
 
 if __name__ == '__main__':
--- day5/tests.py
+++ day5/tests.py
@@ -7,7 +7,7 @@ class Day5Tester(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # (test_input, expected_result
-        cls.test_strings = (('aA', ''), ('abBa', ''), ('abAb', 'abAb'),
+        cls.test_strings = (('aA', ''), ('abBA', ''), ('abAB', 'abAB'),
                             ('aabAAB', 'aabAAB'), ('dabAcCaCBAcCcaDA', 'dabCBAcaDA'))
 
     def test_polymer_reactions(self):
